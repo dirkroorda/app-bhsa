29cb791b8557e452f1666018d1c32a2900046245
Dirk Roorda <dirk@annotation.nl>
2020-04-26 17:10:51 +0200

    even more reduction for v8

--- code/app.py
+++ code/app.py
@@ -1,25 +1,9 @@
-from tf.applib.api import setupApi
+from tf.applib.app import App
 
 
-def notice(app):
-    if int(app.api.TF.version.split(".")[0]) <= 7:
-        print(
-            f"""
-Your Text-Fabric is outdated.
-It cannot load this version of the TF app `{app.appName}`.
-Recommendation: upgrade Text-Fabric to version 8.
-Hint:
-
-    pip3 install --upgrade text-fabric
-
-"""
-        )
-
-
-class TfApp(object):
+class TfApp(App):
     def __init__(app, *args, **kwargs):
-        setupApi(app, *args, **kwargs)
-        notice(app)
+        super().__init__(*args, **kwargs)
 
     def getLexId(app, n):
         api = app.api
